aaae9f1d0b6aba7048a530f22557e11d8b1658a6
AlmaJacob <almajacob7@gmail.com>
2024-10-18 13:39:16 +0530

    Add

---
+++ project/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views 
+urlpatterns=[
+    path('s',views.fun1),
+    path('a',views.fun2),
+    path('',views.fun3),
+    path('fun4/<b>',views.fun4),
+    path('fun5/<int:a>/<int:b>/<int:c>',views.fun5),
+    path('index',views.index_page),
+    path('demo',views.demo),
+    path('second',views.second_page)
+
+]
---
+++ project/app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django. http import HttpResponse
+
+# Create your views here.
+def fun1(request):
+    return HttpResponse ("heyyy")
+
+def fun2(request):
+    return HttpResponse ("heloooo")
+def fun3(request):
+    a={'name':'anu','age':21}
+    return HttpResponse(a)
+def fun4(request,b):
+    return HttpResponse(b)
+def fun5(request,a,b,c):
+    if a<b and a<c:
+        return HttpResponse(a)
+    elif b<a and b<c:
+        return HttpResponse(b)
+    else:
+        return HttpResponse(c)
+def index_page(req):
+    Name='alma'
+    age=21
+    place='ksrgd'
+    return render(req,'index.html',{'Name':Name,'age':age,'place':place})
+
+def demo(req):
+    l={'name':'alma','age':21}
+    d={'name':'alen','age':28}
+    return render(req,'demo.html',{'data1':l},'data2:'d)    
+def second_page(req):
+    return render(req,'second.html')
