3acac4a0d897a2475fe96e7d31a520ccf1103da7
almajacob <almajacob7@gmail.com>
2024-10-30 10:05:22 +0530

    project1 added

---
+++ bookmyshow/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index_page(req):
+    return render(req,'index.html')
+
+def venom_page(req):
+    return render(req,'venom.html')
