77e75e84d1492956312ad7d4cf1766ebe9d6bb52
AlmaJacob <almajacob7@gmail.com>
2024-10-30 14:02:02 +0530

    done

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -8,6 +8,11 @@ urlpatterns=[
 #-----------------------admin----------------------------
 
 path('shop_home',views.shop_home),
+path('add',views.add_product),
+path('edit/<id>',views.edit_pro),
+path('delete/<id>',views.delete_pro),
+
+
 
 
 
